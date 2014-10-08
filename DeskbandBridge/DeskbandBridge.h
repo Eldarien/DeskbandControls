0a15afd83b06d3df921d6708039a4d58efabb57b
Eldarien <Eldarien@5967d7d2-b153-4f49-b960-ced300ac5a46>
2014-10-08 19:20:52 +0000

    version tag
    git-svn-id: http://fractal:81/svn/DeskbandControls@37 5967d7d2-b153-4f49-b960-ced300ac5a46

--- DeskbandBridge/DeskbandBridge.h
+++ DeskbandBridge/DeskbandBridge.h
@@ -4,7 +4,7 @@
 //	const int MAGIC_NUMBER = 42;
 //}
 
-#define DESKBAND_CONTROLS_VERSION "1.1.1"
+#define DESKBAND_CONTROLS_VERSION "1.1.2"
 #define DESKBAND_CONTROLS_TITLE "Deskband Controls"
 
 #define FOOBAR_PLUGIN_MSGWINDOW_CLASS "FoobarDeskbandControlsWndClass"
