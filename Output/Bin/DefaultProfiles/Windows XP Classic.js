639128f2efc3793142a641fc799b7ae42f6b34eb
eldarien <eldarien@5967d7d2-b153-4f49-b960-ced300ac5a46>
2016-01-29 19:47:49 +0000

    VS2015 projects and latest foobar2000 SDK
    git-svn-id: http://fractal:81/svn/DeskbandControls@38 5967d7d2-b153-4f49-b960-ced300ac5a46

---
+++ Output/Bin/DefaultProfiles/Windows XP Classic.js
@@ -0,0 +1,138 @@
+{
+  "General": {
+    "BandSize": 252,
+    "TextScrollSpeed": 300,
+    "DrawControlsOutline": false,
+    "HideIfNotPlaying": false,
+    "FloatingMode": false,
+    "InternetSearchFormat": "$if(%artist%,%artist%' - ')%title%",
+    "InternetSearchUrl": "https://www.google.com/search?q=%q%"
+  },
+  "FloatingWindow": {
+    "Opacity": 1.0,
+    "Color": "#FFD3D3D3",
+    "X": 0,
+    "Y": 0,
+    "Width": 252,
+    "Height": 23,
+    "BackgroundImage": null,
+    "UseBackgroundImage": false,
+    "UseTransparencyKey": true,
+    "StretchBackgroundImage": false
+  },
+  "TextBlocks": [
+    {
+      "Name": "Primary Text",
+      "Format": "%artist% - %title% '('%playback_time%')')",
+      "FontName": "Tahoma",
+      "FontSize": 8.0,
+      "Italic": false,
+      "Bold": true,
+      "FontColor": "#FF000000",
+      "X": 92,
+      "Y": -1,
+      "Width": 158,
+      "Height": 16,
+      "Scroll": true,
+      "AlignToRight": false,
+      "Visible": true,
+      "StoppedText": "**Stopped**"
+    }
+  ],
+  "Buttons": [
+    {
+      "Kind": 0,
+      "X": 30,
+      "Y": -2,
+      "Width": 16,
+      "Height": 16,
+      "Visible": true,
+      "IconPath": null,
+      "AdditionalIconPath": null,
+      "ColorizeColor": "#00FFFFFF"
+    },
+    {
+      "Kind": 1,
+      "X": 44,
+      "Y": -2,
+      "Width": 16,
+      "Height": 16,
+      "Visible": true,
+      "IconPath": null,
+      "AdditionalIconPath": null,
+      "ColorizeColor": "#00FFFFFF"
+    },
+    {
+      "Kind": 2,
+      "X": 59,
+      "Y": -2,
+      "Width": 16,
+      "Height": 16,
+      "Visible": true,
+      "IconPath": null,
+      "AdditionalIconPath": null,
+      "ColorizeColor": "#00FFFFFF"
+    },
+    {
+      "Kind": 3,
+      "X": 74,
+      "Y": -2,
+      "Width": 16,
+      "Height": 16,
+      "Visible": true,
+      "IconPath": null,
+      "AdditionalIconPath": null,
+      "ColorizeColor": "#00FFFFFF"
+    },
+    {
+      "Kind": 4,
+      "X": 0,
+      "Y": -2,
+      "Width": 16,
+      "Height": 16,
+      "Visible": false,
+      "IconPath": null,
+      "AdditionalIconPath": null,
+      "ColorizeColor": "#00FFFFFF"
+    },
+    {
+      "Kind": 5,
+      "X": 0,
+      "Y": -2,
+      "Width": 16,
+      "Height": 16,
+      "Visible": false,
+      "IconPath": null,
+      "AdditionalIconPath": null,
+      "ColorizeColor": "#00FFFFFF"
+    }
+  ],
+  "Trackbars": [
+    {
+      "Kind": 0,
+      "X": 30,
+      "Y": 16,
+      "Width": 156,
+      "Height": 6,
+      "Color": "#FF000000",
+      "Visible": true
+    },
+    {
+      "Kind": 1,
+      "X": 190,
+      "Y": 16,
+      "Width": 60,
+      "Height": 6,
+      "Color": "#FF000000",
+      "Visible": true
+    }
+  ],
+  "AlbumArt": {
+    "X": 1,
+    "Y": -2,
+    "Width": 23,
+    "Height": 23,
+    "Visible": true,
+    "StubImagePath": null
+  }
+}
